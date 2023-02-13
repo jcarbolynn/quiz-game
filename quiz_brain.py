f0de44b992e19689a2aff88b957387b31a1a1d18
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-02-13 15:04:31 -0500

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,41 +1,33 @@
+import html
+
 class QuizBrain:
+
   def __init__(self, q_list):
     self.question_number = 0
     self.score = 0
     self.question_list = q_list
-    
-  # my attempt:(
-  # def next_question(self, q_num, q_list):
-  #   answer = input(f"{self.q_list[self.q_num].text}")
-  #   return answer
+    self.current_question = None
+
+  def still_has_questions(self):
+    return self.question_number < len(self.question_list)
 
   def next_question(self):
-    current_question = self.question_list[self.question_number]
-    # starts at zero
+    self.current_question = self.question_list[self.question_number]
     self.question_number += 1
-    user_answer = input(f"Q. {self.question_number}: {current_question.text} (True/False): ")
-    self.check_answer(user_answer, current_question.answer)
+    q_text = html.unescape(self.current_question.text)
+    # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
+    # self.check_answer(f"Q.{self.question_number}: {q_text} (True/False): ")
+    return(f"Q.{self.question_number}: {q_text}")
 
-  def still_has_question(self):
-    return self.question_number < len(self.question_list)
-    # if self.question_number < len(self.question_list):
-    #   return True
-    # else:
-    #   return False
-
-    
-    # if self.question_list[self.question_number] == self.question_list.length():
-    #   return False
-    # else:
-    #   return True
-
-  def check_answer(self, user_answer, correct_answer):
+  def check_answer(self, user_answer) -> bool:
+    correct_answer = self.current_question.answer
     if user_answer.lower() == correct_answer.lower():
       self.score += 1
       print("You got it right!")
+      return True
     else:
-      print("You got it wrong")
-    print(f"The correct answer was {correct_answer}.")
-    print(f"Your current score is: {self.score}/{self.question_number}\n")
-    
-    
+      print("That's wrong.")
+      return False
+
+      # print(f"Your current score is: {self.score}/{self.question_number}")
+      # print("\n")
